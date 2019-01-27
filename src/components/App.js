ffc878e01a330001adee958c856d7dfc967faa30
vjself <vincent@Vincents-MacBook-Pro.local>
2019-01-26 23:03:51 -0700

    finished project with no errors

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,11 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import axios from "axios";
 
-import './App.css';
+import "./App.css";
 
-import Header from './Header/Header';
-import Compose from './Compose/Compose';
+import Header from "./Header/Header";
+import Compose from "./Compose/Compose";
+import Post from "./Post/Post";
 
 class App extends Component {
   constructor() {
@@ -13,25 +15,39 @@ class App extends Component {
       posts: []
     };
 
-    this.updatePost = this.updatePost.bind( this );
-    this.deletePost = this.deletePost.bind( this );
-    this.createPost = this.createPost.bind( this );
+    this.updatePost = this.updatePost.bind(this);
+    this.deletePost = this.deletePost.bind(this);
+    this.createPost = this.createPost.bind(this);
   }
-  
+
   componentDidMount() {
-
+    axios.get("https://practiceapi.devmountain.com/api/posts").then(results => {
+      this.setState({ posts: results.data });
+    });
   }
 
-  updatePost() {
-  
+  updatePost(id, text) {
+    axios
+      .put(`https://practiceapi.devmountain.com/api/posts?id=${id}`, { text })
+      .then(results => {
+        this.setState({ posts: results.data });
+      });
   }
 
-  deletePost() {
-
+  deletePost(id) {
+    axios
+      .delete(`https://practiceapi.devmountain.com/api/posts?id=${id}`)
+      .then(results => {
+        this.setState({ posts: results.data });
+      });
   }
 
-  createPost() {
-
+  createPost(text) {
+    axios
+      .post("https://practiceapi.devmountain.com/api/posts", { text })
+      .then(results => {
+        this.setState({ posts: results.data });
+      });
   }
 
   render() {
@@ -42,9 +58,18 @@ class App extends Component {
         <Header />
 
         <section className="App__content">
+          <Compose createPostFn={this.createPost} />
 
-          <Compose />
-          
+          {posts.map(post => (
+            <Post
+              key={post.id}
+              id={post.id}
+              text={post.text}
+              date={post.date}
+              updatePostFn={this.updatePost}
+              deletePostFn={this.deletePost}
+            />
+          ))}
         </section>
       </div>
     );
--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,13 +1,13 @@
-import React, { Component } from 'react';
-import ProfileIcon from 'react-icons/lib/md/person-outline';
-import ReplyIcon from 'react-icons/lib/md/chat-bubble-outline';
-import FavoriteIcon from 'react-icons/lib/md/favorite-outline';
-import MessageIcon from 'react-icons/lib/md/mail-outline';
-import MasterControlIcon from 'react-icons/lib/md/more-vert';
+import React, { Component } from "react";
+import ProfileIcon from "react-icons/lib/md/person-outline";
+import ReplyIcon from "react-icons/lib/md/chat-bubble-outline";
+import FavoriteIcon from "react-icons/lib/md/favorite-outline";
+import MessageIcon from "react-icons/lib/md/mail-outline";
+import MasterControlIcon from "react-icons/lib/md/more-vert";
 
-import './Post.css';
+import "./Post.css";
 
-import Edit from './Edit/Edit';
+import Edit from "./Edit/Edit";
 
 //////////////////////////////////////////////////////// THIS COMPONENT IS BEING RENDERED IN THE *APP* COMPONENT
 
@@ -20,10 +20,10 @@ export default class Post extends Component {
       showMasterMenu: false
     };
 
-    this.hideEdit = this.hideEdit.bind( this );
-    this.showEdit = this.showEdit.bind( this );
-    this.toggleMasterMenu = this.toggleMasterMenu.bind( this );
-    this.hideMasterMenu = this.hideMasterMenu.bind( this );
+    this.hideEdit = this.hideEdit.bind(this);
+    this.showEdit = this.showEdit.bind(this);
+    this.toggleMasterMenu = this.toggleMasterMenu.bind(this);
+    this.hideMasterMenu = this.hideMasterMenu.bind(this);
   }
 
   // This puts the post into EDIT mode when the EDIT button is clicked from the drop-down
@@ -44,33 +44,32 @@ export default class Post extends Component {
 
   // This hides the drop-down when the post is clicked anywhere
   hideMasterMenu() {
-    if ( this.state.showMasterMenu === true ) {
+    if (this.state.showMasterMenu === true) {
       this.setState({ showMasterMenu: false });
     }
   }
 
   render() {
-    // This is destructuring! You can also think of it as being written as so:
-      // const editing = this.state.editing
-      // const showMasterMenu = this.state.showMasterMenu
     const { editing, showMasterMenu } = this.state;
+    const { text, date } = this.props;
 
     return (
-      // Main body of post
-      <section className="Post__parent" onClick={ this.hideMasterMenu }>
-
-        {/* Three dots in top right corner */}
+      <section className="Post__parent" onClick={this.hideMasterMenu}>
         <div className="Post__master-controls">
-          <MasterControlIcon onClick={ this.toggleMasterMenu } />
+          <MasterControlIcon onClick={this.toggleMasterMenu} />
 
-          {/* Drop-down menu. Remember that the "showMasterMenu" variable has been destructured off of this.state */}
-          <div className="Post__master-menu" style={ { display: showMasterMenu ? 'flex' : 'none' } }>
-            <span onClick={ this.showEdit }>Edit</span>
-            <span>Delete</span>
+          <div
+            className="Post__master-menu"
+            style={{ display: showMasterMenu ? "flex" : "none" }}
+          >
+            <span onClick={this.showEdit}>Edit</span>
+            <span onClick={() => this.props.deletePostFn(this.props.id)}>
+              Delete
+            </span>{" "}
+            {/* Remember to destructure deletePostFn off of props or use this.props.deletePostFn */}
           </div>
         </div>
 
-        {/* This is where all the meta data of the post will go (who, when, where) */}
         <div className="Post__meta-data">
           <div className="Post__profile-picture">
             <ProfileIcon />
@@ -79,37 +78,27 @@ export default class Post extends Component {
           <span className="Post__name">DevMountain</span>
           <span className="Post__handle">@DevMountain</span>
 
-          <span className="Post__date">- POST DATE GOES HERE</span>
+          <span className="Post__date">- {date}</span>
         </div>
 
-        {/* This is where the text goes. Notice the turnary statement. The turnary statement decides to display either the text OR the editor view
-            You can also think of it as being written as so:
-              if( this.state.editing === true ) {
-                <Edit ... />
-              } else {
-                <span ... ></span>
-              }
-        */}
         <div className="Post__content">
-          {
-            // This has been pulled off of this.state via destructuring
-            editing
-            ?
-              <Edit text=""
-                    hideEdit={ this.hideEdit } />
-            :
-              <span className="Post__text">POST TEXT GOES HERE</span>
-          }
+          {editing ? (
+            <Edit
+              text={text}
+              id={this.props.id} // Remember to destructure id off of props or use this.props.id
+              hideEdit={this.hideEdit}
+              updatePostFn={this.props.updatePostFn}
+            />
+          ) : (
+            <span className="Post__text">{text}</span>
+          )}
         </div>
-
-        {/* These are all of the cute little icons in the bottom left corner */}
         <div className="Post__user-controls">
           <ReplyIcon className="Post__control-icon" />
           <FavoriteIcon className="Post__control-icon" />
           <MessageIcon className="Post__control-icon" />
         </div>
-
       </section>
-    )
+    );
   }
 }
